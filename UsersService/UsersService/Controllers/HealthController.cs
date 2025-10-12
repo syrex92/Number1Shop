a836fbd514b8b4d046b5f7b325b4040a2c3ece0a
IGEG <igor127@bk.ru>
2025-10-12 20:24:07 +0300

    [#UserService][Update]Added Health controller

---
+++ UsersService/UsersService/Controllers/HealthController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealhController : ControllerBase
+    {
+        public HealhController()
+        {
+        }
+        /// <summary>
+        /// Проверка доступности сервиса
+        /// </summary>
+        /// <remarks>
+        /// Вызывается для проверки доступности сервиса. Если возвращён статус 200 - значит сервис доступен.
+        /// Применяется для контроля состояния сервиса в контейнере
+        /// </remarks>
+        /// <returns>plain string message</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [AllowAnonymous]
+        public IActionResult CheckHealth()
+        {
+            return Ok(new
+            {
+                Status = "Cart service working",
+                Timestamp = DateTime.UtcNow,
+                Service = "User Service",
+            });
+        }
+    }
+}
