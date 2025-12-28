b2016eb34fe9614d86bea8a01b5babd6a71afd94
Paroxizm <readmishko@yandex.ru>
2025-12-28 07:56:11 +0300

    move to branch

--- Shop/vite.config.ts
+++ Shop/vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
+    server: {
+        port: 8080,
+        strictPort: true,
+        host: true,
+        origin: "http://localhost:8080",
+    },
     //envPrefix: ""
 })
